f61126d7e466153eec18543fbc109b106b7740bd
thill <thill@tntech.edu>
2022-04-17 12:10:01 -0500

    switched extruder to 1/4 stepping

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -988,7 +988,7 @@
 //#define MICROSTEP32 HIGH,LOW,HIGH
 
 // Microstep settings (Requires a board with pins named X_MS1, X_MS2, etc.)
-#define MICROSTEP_MODES { 16, 16, 16, 16, 8, 16 } // [1,2,4,8,16] // TH changed both extruders to 8 (1/8 stepping) for E3D 0.9deg/step 'slimline' motor 
+#define MICROSTEP_MODES { 16, 16, 16, 16, 4, 16 } // [1,2,4,8,16] // TH changed extruder1 to 4 (1/4 stepping) for E3D 0.9deg/step 'slimline' motor -> this should increase torque
 
 /**
  *  @section  stepper motor current
